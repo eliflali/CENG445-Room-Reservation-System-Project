f99315ce9465c155b19bad35da0d42e99f0ed016
Elif Lale <63200204+eliflali@users.noreply.github.com>
2023-11-22 05:34:31 +0300

    Update models.py

--- phase-1/models.py
+++ phase-1/models.py
@@ -1,9 +1,11 @@
+import datetime
+from datetime import datetime, timedelta
 import uuid
 import hashlib
 import secrets
-
-
 import faker
+
+
 
 class SingletonMeta(type):
     """
@@ -29,6 +31,8 @@ class UserManager(metaclass=SingletonMeta):
             print ("Current user set to: ", self.current_user.get())
 
     def switch_user(self, user_id):
+        print("line 31", user_id)
+        print(self.users)
         if user_id in self.users:
             self.current_user = self.users[user_id]
             print ("User switched to: ", self.current_user.get())
@@ -40,7 +44,7 @@ class UserManager(metaclass=SingletonMeta):
     
     def get_user(self, user_id):
         return self.users.get(user_id)
-    
+
     def get_users(self):
         return self.users
 
@@ -103,13 +107,17 @@ class User(CRUD):
         
 class Room(CRUD):
     def __init__(self, user, name, x, y, capacity, working_hours, permissions):
-        super().__init__(user=user, 
+        super().__init__(user=user,  
                         name=name, 
                         x=x, 
-                        y=y, 
+                        y=y,
                         capacity=capacity, 
                         working_hours=working_hours, 
                         permissions=permissions)
+        self.reservations = []  
+
+    def get_name(self):
+        return self.name
 
     def get_permissions(self):
         return self.permissions
@@ -122,9 +130,59 @@ class Room(CRUD):
     
     def get_permissions(self):
         return self.permissions
-
+    
     def get_id(self):
         return self.id
+
+    def is_in_rectangle(self, rect):
+        #rect is defined as (x,y,w,h) 
+        #rect[0] = x - min x, rect[1] = y - max y, rect[2] = w - max x, rect[3] = h - min y
+        #x y are the coordinates of the bottom left corner. 
+        #Top right corner will be x+w,y+h
+        if(self.x > rect[0] and self.x < rect[2] and self.y > rect[3] and self.y < rect[1]):
+            return True
+        
+        return False
+
+    def is_available(self, start_time, duration):
+        print("start_time:", type(start_time))
+        #print("datetime.timedelta(minutes=duration):", type(datetime.timedelta(minutes=duration)))
+        if(type(duration) is str):
+            duration_parts = duration.split(', ')
+            duration_delta = timedelta()
+
+            for part in duration_parts:
+                value, unit = part.split()
+                if "day" in unit:
+                    duration_delta += timedelta(days=int(value))
+                elif "hour" in unit:
+                    duration_delta += timedelta(hours=int(value))
+                elif "minute" in unit:
+                    duration_delta += timedelta(minutes=int(value))
+        else:
+            duration_delta = duration
+        end_time = start_time + duration_delta
+
+        if not self.is_working_hours(start_time, end_time):
+            return False
+        
+        for reservation in self.reservations:
+            reservation_start, reservation_end = reservation
+            # Check for overlap between reservations
+            if start_time < reservation_end and end_time > reservation_start:
+                return False  # Room is already reserved during this time
+
+        # If no conflicts were found, the room is available
+        print("line 174")
+        return True
+
+    def is_working_hours(self, start_time, end_time):
+        workhours_start_time_str, workhours_end_time_str = self.working_hours.split('-')
+        workhours_start_time = datetime.strptime(workhours_start_time_str, "%H.%M").time()
+        workhours_end_time = datetime.strptime(workhours_end_time_str, "%H.%M").time()
+        # Check if the event's time interval falls within the room's working hours
+        return workhours_start_time <= start_time.time() <= workhours_end_time and \
+               workhours_start_time <= end_time.time() <= workhours_end_time
     
 class Event(CRUD):
     def __init__(self, title, description, category, capacity, duration, weekly, permissions):
@@ -134,11 +192,12 @@ class Event(CRUD):
                         capacity=capacity, 
                         duration=duration, 
                         weekly=weekly, 
-                        permissions=permissions, 
-                        location = None, start_time = None)
+                        permissions=permissions,
+                        room_id = None,
+                        start_time = None)
 
-    def reserve_event(self, location, start_time):
-        self.location = location
+    def reserved_event(self, room_id, start_time):
+        self.room_id = room_id
         self.start_time = start_time
     
     def get_permissions(self):
@@ -162,10 +221,12 @@ class Event(CRUD):
 
 class Organization(CRUD):
     objects = {}  # Dictionary to store Organization objects
-    
+
     #map changed to mapOrganization since map is reserved word
     def __init__(self, owner, name, mapOrganization, backgroundImage = None):
-        super().__init__(name=name, mapOrganization=mapOrganization, backgroundImage=backgroundImage)
+        super().__init__(name=name, 
+                        mapOrganization=mapOrganization, 
+                        backgroundImage=backgroundImage)
         self.user_manager = owner #UserManager is singleton instance.
         self.owner = owner.get_current_user()
         self.rooms = {}
@@ -175,7 +236,7 @@ class Organization(CRUD):
     def listobjects(cls):
         # List all objects of this class
         return cls.objects
-    
+
     def delete(self):
         for room in self.rooms:
             room.delete_room()
@@ -192,11 +253,12 @@ class Organization(CRUD):
         new_room = Room(current_user, name, x, y, capacity, working_hours, permissions)
         id = new_room.get_id()
         self.rooms[id] = new_room
-        self.objects[id] = f"room_{new_room.get()[name]}"
-        return id
+        self.objects[id] = f"room_{new_room.get_name}"
+        return new_room
 
     def get_room(self, room_id):
         return self.rooms.get(room_id)
+    
     
     #Update for room
     def update_organization_room(self, room_id, **kwargs):
@@ -214,6 +276,7 @@ class Organization(CRUD):
         else:
             raise ValueError(f"No room found with ID {room_id}")
 
+    
     def create_organization_event(self, title, description, category, capacity, duration, weekly, permissions):
         current_user = self.user_manager.get_current_user()
         if current_user is None:
@@ -221,18 +284,18 @@ class Organization(CRUD):
         new_event = Event(title, description, category, capacity, duration, weekly, permissions)
         id = new_event.get_id()
         self.events[id] = new_event
-        self.objects[id] = f"event_{new_event.get()[title]}"
-        return id
-    
+        self.objects[id] = f"event_{new_event.title}"
+        return new_event
+
     def get_event(self, event_id):
         return self.events.get(event_id)
-    
+
     def update_organization_event(self, event_id, **kwargs):
         if event_id in self.events:
             return self.events[event_id].update_event(**kwargs)
         else:
             raise ValueError(f"No event found with ID {event_id}")
-    
+
     def delete_organization_event(self, event_id):
         if event_id in self.events:
             self.events[event_id].delete_event()
@@ -241,21 +304,149 @@ class Organization(CRUD):
         else:
             raise ValueError(f"No event found with ID {event_id}")
 
-    def reserve(self, event_title, room_name, start_time):
-        pass
+    #taslak - no permissions included
+    def reserve(self, event, room, start_time):
+        # Check if the room is available for the specified time
+        if not room.is_available(start_time, event.get_duration()):
+            raise ValueError("Room is not available for the specified time.")
 
+        # Check if the event is already assigned to a room
+        if event.room_id is not None:
+            raise ValueError("Event is already assigned to a room.")
+
+        if(("WRITE" not in event.get_permissions() 
+            or 
+            "WRITE" not in room.get_permissions()) 
+            and
+            ("all" not in event.get_permissions()
+            or
+            "all" not in room.get_permissions())
+            ):
+            raise ValueError("User does not have permission.")
+        
+        if(type(event.weekly) is datetime 
+            and 
+            ("PERWRITE" not in room.get_permissions() or "all" not in room.get_permissions())):
+            raise ValueError("User does not have permission for weekly events.")
+
+
+        # Assign the room to the event and update its start time
+        event.reserved_event(room.get_id, start_time)
+
+        # Add the reservation to the room's reservations list
+        duration_parts = event.get_duration().split(', ')
+        duration_delta = timedelta()
+
+        for part in duration_parts:
+            value, unit = part.split()
+            if "day" in unit:
+                duration_delta += timedelta(days=int(value))
+            elif "hour" in unit:
+                duration_delta += timedelta(hours=int(value))
+            elif "minute" in unit:
+                duration_delta += timedelta(minutes=int(value))
+
+        room.reservations.append((start_time, start_time + duration_delta))
+
+        # Return a success message or reservation details
+        return f"Room {room.name} reserved for event {event.title} starting at {start_time}."
+
+    #rect is defined as (x,y,w,h) 
+    #x y are the coordinates of the bottom left corner. 
+    #Top right corner will be x+w,y+h
     def find_room(self, event, rect, start, end):
-        pass
+        available_rooms = []
+
+        for room_id, room in self.rooms.items():
+            if room.is_in_rectangle(rect):
+                print("inrectangle")
+                if room.is_available(start, end - start):
+                    "available"
+                    if room.capacity >= event.get_capacity():
+                        "capacity ok"
+                        available_rooms.append(room)
+
+        return available_rooms
     
     def find_schedule(self, eventlist, rect, start, end):
-        pass
+        schedule = {}
+        for event in eventlist:
+            available_rooms = self.find_room(event, rect, start, end)
 
-    def reassign(event, room):
-        pass
+            for room in available_rooms:
+                current_time = start
+                while current_time < end:
+                    # Calculate event end time
+                    duration_parts = event.get_duration().split(', ')
+                    duration_delta = timedelta()
+                    for part in duration_parts:
+                        value, unit = part.split()
+                        if "day" in unit:
+                            duration_delta += timedelta(days=int(value))
+                        elif "hour" in unit:
+                            duration_delta += timedelta(hours=int(value))
+                        elif "minute" in unit:
+                            duration_delta += timedelta(minutes=int(value))
+
+                    event_end_time = current_time + duration_delta
+
+                    # Check if the room is available for the event duration
+                    if room.is_available(current_time, event_end_time - current_time):
+                        # Schedule the event
+                        schedule[event.id] = {'room_id': room.id, 'start_time': current_time}
+                        break
+
+                    # Increment current time
+                    current_time += timedelta(minutes=15)  # Adjust the time increment as needed
+
+                if event.id not in schedule:
+                    return None  # Unable to schedule one of the events
+
+        return schedule
+
+    def reassign(self, event, room, start_time):
+        # Check if the room is available for the specified time
+        if not room.is_available(start_time, event.get_duration()):
+            raise ValueError("Room is not available for the specified time.")
+
+        if(("WRITE" not in event.get_permissions() 
+            or 
+            "WRITE" not in room.get_permissions()) 
+            and
+            ("all" not in event.get_permissions()
+            or
+            "all" not in room.get_permissions())
+            ):
+            raise ValueError("User does not have permission.")
+        
+        if(type(event.weekly) is datetime 
+            and 
+            ("PERWRITE" not in room.get_permissions() or "all" not in room.get_permissions())):
+            raise ValueError("User does not have permission for weekly events.")
+
+        # Assign the room to the event and update its start time
+        event.reserved_event(room.get_id, start_time)
+
+        duration_parts = event.get_duration().split(', ')
+        duration_delta = timedelta()
+
+        for part in duration_parts:
+            value, unit = part.split()
+            if "day" in unit:
+                duration_delta += timedelta(days=int(value))
+            elif "hour" in unit:
+                duration_delta += timedelta(hours=int(value))
+            elif "minute" in unit:
+                duration_delta += timedelta(minutes=int(value))
+
+        room.reservations.append((start_time, start_time + duration_delta))
+
+
+        # Return a success message or reservation details
+        return f"Room {room.name} reserved(reassigned) for event {event.title} starting at {start_time}."
     
     def query(rect, title, category, room):
         pass
-
 
 def create_fake_data():
     fake = faker.Faker()
@@ -263,11 +454,35 @@ def create_fake_data():
     user1 = User(username="user1", email=fake.email(), fullname=fake.name(), passwd="123")
     user2 = User(username="user2", email=fake.email(), fullname=fake.name(), passwd="123")
     user3 = User(username="user3", email=fake.email(), fullname=fake.name(), passwd="123")
+
+    user_manager.switch_user(user1.get_id())
     
-    org1 = Organization(user1, name="org1", mapOrganization="map1")
+    org1 = Organization(user_manager, name="org1", mapOrganization="map1")
+    time_str0 = "01-01-2023,15:30"
+    time_str1 = "12-03-2023,15:30"
+    time1 = datetime.strptime(time_str1, "%d-%m-%Y,%H:%M")
+    time0 = datetime.strptime(time_str0, "%d-%m-%Y,%H:%M")
+
+    duration = "15 minutes"
+
+    room1 = org1.create_organization_room(name="room1", x=1, y=1, capacity=10, working_hours="09.00-17.00", permissions="all")
+    room2 = org1.create_organization_room(name="room2", x=2, y=2, capacity=20, working_hours="09.00-17.00", permissions="all")
+    event1 = org1.create_organization_event(title="event1", description="desc1", category="cat1", capacity=10, duration=duration, weekly=True, permissions="all")   
+    event2 = org1.create_organization_event(title="event2", description="desc2", category="cat2", capacity=20, duration=duration, weekly=True, permissions="all")
+
+    #rect is defined as (x,y,w,h) 
+    #rect[0] = x - min x, rect[1] = y - max y, rect[2] = w - max x, rect[3] = h - min y
+    #x y are the coordinates of the bottom left corner. 
+    #Top right corner will be x+w,y+h
+    print("line 427", org1.find_room(event1, (0,100,100,0), time0, time1))
+    print(org1.reserve(event1, room1, time1))
+    print(org1.reassign(event1, room2, time1))
+    print(org1.find_schedule([event1, event2], (0,100,100,0), time0, time1))
+
+def test_organization():
+    create_fake_data()
     
-    org1.create_organization_room(name="room1", x=1, y=1, capacity=10, working_hours="9-5", permissions="all")
-    org1.create_organization_room(name="room2", x=2, y=2, capacity=20, working_hours="9-5", permissions="all")
-    org1.create_organization_event(title="event1", description="desc1", category="cat1", capacity=10, duration=2, weekly=True, permissions="all")   
-    org1.create_organization_event(title="event2", description="desc2", category="cat2", capacity=20, duration=2, weekly=True, permissions="all")
-    
+test_organization()
+
+
+
