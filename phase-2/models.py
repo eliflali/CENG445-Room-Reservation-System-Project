e880119efad6d9e69bfef7b21809d45552e858f2
“elifNB” <elifNB@neuralbridge.ai>
2023-12-09 20:15:29 +0300

    deleted stg

--- phase-2/models.py
+++ phase-2/models.py
@@ -437,24 +437,4 @@ class View:
 
 
 
-from abc import ABC, abstractmethod
-import uuid
-
-class CRUD(ABC): # Inherit from ABC(Abstract base class)
-    @abstractmethod
-    def __init__(self) -> None:
-        pass
-
-    @abstractmethod
-    def get(self) -> dict:
-        pass
-
-    @abstractmethod
-    def update(self) -> None:
-        pass
-
-    @abstractmethod
-    def delete(self) -> None:
-        pass
-
 
